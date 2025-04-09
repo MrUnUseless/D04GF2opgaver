52e4ff3294d209ccbcaba4e902a1fb1d112b3a41
rasmu <rasmu@LERASMUSNOVO>
2025-04-09 13:20:38 +0200

    Add project files.

---
+++ D04GF2opgaver/Opgaver/OpgaveSkabelon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D04GF2opgaver.Opgaver
+{
+    class Opgave01
+    {
+        public static void OpgaveRun()
+        {
+            Console.WriteLine("opgave test");
+        }
+    }
+}
---
+++ D04GF2opgaver/Program.cs
@@ -0,0 +1,13 @@
+﻿using D04GF2opgaver.Opgaver;
+
+namespace D04GF2opgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Opgave01.OpgaveRun();
+            Console.ReadKey();
+        }
+    }
+}
